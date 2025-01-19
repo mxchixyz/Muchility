cfbf90f83a3a6b422e3c3b954a4d16f62a8be497
Muchi <vertuescloud@gmail.com>
2025-01-19 17:48:41 +1300

    Update Muchility Debloater.ps1

--- Muchility Debloater.ps1
+++ Muchility Debloater.ps1
@@ -4,77 +4,42 @@
 #╚═╩═╩═╝╚═══╝╚═══╝╚═╝╚═╝╚═╝"
 
 
-$GoodApps =	"calculator|store|windowsnotepad"
-
+$GoodApps = "calculator|store|windowsnotepad"
 
 Function RemoveApps {
-	#SafeApps contains apps that shouldn't be removed, or just can't and cause errors
-	$SafeApps = "AAD.brokerplugin|accountscontrol|apprep.chxapp|assignedaccess|asynctext|bioenrollment|capturepicker|cloudexperience|contentdelivery|desktopappinstaller|ecapp|edge|extension|getstarted|immersivecontrolpanel|lockapp|net.native|oobenet|parentalcontrols|PPIProjection|search|sechealth|secureas|shellexperience|startmenuexperience|terminal|vclibs|xaml|XGpuEject"
-	If ($Xbox) {
-		$SafeApps = "$SafeApps|Xbox" 
+    # SafeApps contains apps that shouldn't be removed, or just can't and cause errors
+    $SafeApps = "AAD.brokerplugin|accountscontrol|apprep.chxapp|assignedaccess|asynctext|bioenrollment|capturepicker|cloudexperience|contentdelivery|desktopappinstaller|ecapp|edge|extension|getstarted|immersivecontrolpanel|lockapp|net.native|oobenet|parentalcontrols|PPIProjection|search|sechealth|secureas|shellexperience|startmenuexperience|terminal|vclibs|xaml|XGpuEject"
+    
+    If ($Xbox) {
+        $SafeApps = "$SafeApps|Xbox"
+    }
+    
+    If ($Allapps) {
+        $RemoveApps = Get-AppxPackage -allusers | where-object {$_.name -notmatch $SafeApps}
+        $RemovePrApps = Get-AppxProvisionedPackage -online | where-object {$_.displayname -notmatch $SafeApps}
+        
+        ForEach ($RemovedApp in $RemoveApps) {
+            Remove-AppxPackage -package $RemovedApp -erroraction silentlycontinue
+        }
+        
+        ForEach ($RemovedPrApp in $RemovePrApps) {
+            Remove-AppxProvisionedPackage -online -packagename $RemovedPrApp.packagename -erroraction silentlycontinue
+        }
+    } Else {
+        $SafeApps = "$SafeApps|$GoodApps"
+        $RemoveApps = Get-AppxPackage -allusers | where-object {$_.name -notmatch $SafeApps}
+        $RemovePrApps = Get-AppxProvisionedPackage -online | where-object {$_.displayname -notmatch $SafeApps}
+        
+        ForEach ($RemovedApp in $RemoveApps) {
+            Remove-AppxPackage -package $RemovedApp -erroraction silentlycontinue
+        }
+        
+        ForEach ($RemovedPrApp in $RemovePrApps) {
+            Remove-AppxProvisionedPackage -online -packagename $RemovedPrApp.packagename -erroraction silentlycontinue
+        }
+    }
 }
-	
-	If ($Allapps) {
-		$RemoveApps = Get-AppxPackage -allusers | where-object {$_.name -notmatch $SafeApps}
-		$RemovePrApps = Get-AppxProvisionedPackage -online | where-object {$_.displayname -notmatch $SafeApps}
-			ForEach ($RemovedApp in $RemoveApps) {
-				Remove-AppxPackage -package $RemovedApp -erroraction silentlycontinue
-				
-}			ForEach ($RemovedPrApp in $RemovePrApps) {
-				Remove-AppxProvisionedPackage -online -packagename $RemovedPrApp.packagename -erroraction silentlycontinue
-				
-}
-}	Else {
-		$SafeApps = "$SafeApps|$GoodApps"
-		$RemoveApps = Get-AppxPackage -allusers | where-object {$_.name -notmatch $SafeApps}
-		$RemovePrApps = Get-AppxProvisionedPackage -online | where-object {$_.displayname -notmatch $SafeApps}
-			ForEach ($RemovedApp in $RemoveApps) {
-				Remove-AppxPackage -package $RemovedApp -erroraction silentlycontinue
-				
-}			ForEach ($RemovedPrApp in $RemovePrApps) {
-				Remove-AppxProvisionedPackage -online -packagename $RemovedPrApp.packagename -erroraction silentlycontinue
-				
-}
-}
-} 
-#End Function RemoveApps
+# End Function RemoveApps
 
-#Clean up the default start menu
-Function ClearStartMenu {
-    If ($ClearStart) {
-		Write-Host "***Setting empty start menu for new profiles...***"
-#Don't edit this. Creates empty start menu if -ClearStart is used.
-        $StartLayoutStr = @"
-<LayoutModificationTemplate Version="1" xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification" xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout" xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout" xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout">
-  <LayoutOptions StartTileGroupCellWidth="6" />
-  <DefaultLayoutOverride>
-    <StartLayoutCollection>
-      <defaultlayout:StartLayout GroupCellWidth="6" xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout">
-      </defaultlayout:StartLayout>
-    </StartLayoutCollection>
-  </DefaultLayoutOverride>
-  </LayoutModificationTemplate>
-"@
-	    add-content $Env:TEMP\startlayout.xml $StartLayoutStr
-        import-startlayout -layoutpath $Env:TEMP\startlayout.xml -mountpath $Env:SYSTEMDRIVE\
-        remove-item $Env:TEMP\startlayout.xml
-}    Else {        
-		Write-Host "***Setting clean start menu for new profiles...***"
-#Custom start layout XML near the top of the script.
+RemoveApps
 
-        add-content $Env:TEMP\startlayout.xml $StartLayoutStr
-        import-startlayout -layoutpath $Env:TEMP\startlayout.xml -mountpath $Env:SYSTEMDRIVE\
-        remove-item $Env:TEMP\startlayout.xml
-}
-}
-
-
-	Start-Sleep -Seconds 5
-	RemoveApps
-    ClearStartMenu
-    goodbye
-
- function goodbye {
-clear-host
-exit
-}
